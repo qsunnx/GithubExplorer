9ac77975b2f6fae4f1159b257458010db4ddeaa1
Qsunnx <qsunnx@gmail.com>
2019-11-24 14:52:34 +0300

    Хранение данных в keychain

---
+++ GitHubExplorer/Model/SecureManager.swift
@@ -0,0 +1,13 @@
+//
+//  SecureManager.swift
+//  GitHubExplorer
+//
+//  Created by Кирилл Юдин on 20.11.2019.
+//  Copyright © 2019 Кирилл Юдин. All rights reserved.
+//
+
+import Foundation
+
+class SecureManager {
+    
+}
